4b18b2c0c108d85e33737d825f97c4c71661f25a
Seena <seenarowhani95@gmail.com>
2017-04-04 14:57:26 -0400

    add(notif): Add notif example as ref

--- navigation/routes/home.js
+++ navigation/routes/home.js
@@ -1,11 +1,11 @@
-import { TextInput, ScrollView, View} from 'react-native'
+import { TextInput, ScrollView, View, StyleSheet} from 'react-native'
 import { Content, List, ListItem, Text } from 'native-base'
 import React, {Component} from 'react'
 import KeyboardSpacer from 'react-native-keyboard-spacer';
 import {
   withNavigation
 } from '@expo/ex-navigation'
-
+import { Notifications } from 'expo'
 const io = require('socket.io-client')
 
 @withNavigation
@@ -15,26 +15,58 @@ export default class Home extends Component {
       title: 'ws-chat'
     }
   }
-
   state = {
-    messages: [{
-      name: 'Seena',
-      message: 'Welcome'
-    }],
+    messages: [],
     hasConnectivity: false,
     text: ''
   }
-  componentDidMount (props) {
+  componentWillMount (props) {
+    this._notifManager = Notifications.addListener(
+      this._notifHandler.bind(this)
+    )
     this.socket = io('https://server-yzgtsbjhmp.now.sh')
-
-    this.socket.on('connect', () => this.setState({ hasConnectivity: true }))
+    this.socket.on('connect', () => {
+      requestIdleCallback(() => {
+        this.setState({
+          messages: this.state.messages.concat({
+            message: `Successfully connected`
+          }),
+          hasConnectivity: true
+        })
+      })
+    })
 
     this.socket.on('recieve', (message) => {
       requestIdleCallback(() => {
         this.setState({messages: this.state.messages.concat(message)})
+        Notifications.presentLocalNotificationAsync({
+          title: `New message from ${message.name}`,
+          body: `${message.message}`,
+          data: {
+            body: message,
+          },
+          ios: {
+            sound: true,
+          },
+          android: {
+            vibrate: true,
+          },
+        })
       })
     })
   }
+
+  componentWillUnmount () {
+    this._notifManager.remove();
+  }
+
+  _notifHandler (notification) {
+    this.props.navigator.showLocalAlert(
+      notification.data.body.message,
+      notificationStyle
+    )
+  }
+
   _submit () {
     requestIdleCallback(() => {
       this.socket.emit('send', this.state.text)
@@ -56,7 +88,7 @@ export default class Home extends Component {
         <View>
           <TextInput
             placeholder='Send'
-            editable={this.hasConnectivity}
+            editable={this.state.hasConnectivity}
             onChangeText={(text) => this.setState({text})}
             onSubmitEditing={() => this._submit()}
             style={{bottom: 0, padding: 10}}
@@ -77,3 +109,12 @@ export default class Home extends Component {
     )
   }
 }
+
+const notificationStyle = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff'
+  },
+  text: {
+    color: `#${(Math.random() * 0xFFFFFF << 0).toString(16)}`
+  }
+})
